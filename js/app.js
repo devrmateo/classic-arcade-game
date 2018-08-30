2ec28d918af061c265018fbc7a7e075094360baf
Matthew Smith <mateoelcule@gmail.com>
2018-08-30 16:43:32 -0700

    Moved varialbes and functionality from Enemy.prototype render to Enemy (except for drawing)

--- js/app.js
+++ js/app.js
@@ -5,20 +5,7 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    const canvasHeight = document.querySelector('canvas').height;
+    const canvasHeight = 606;
     const numRows = 6;
     const rowPadding = 30;
     const rowHeight = canvasHeight / numRows - rowPadding;
@@ -28,7 +15,23 @@ Enemy.prototype.render = function() {
     const xCoords = [xCoordRow1, xCoordRow2, xCoordRow3];
     this.x = 0;
     this.y = xCoords[Math.floor(Math.random() * xCoords.length)];
+    this.speed = Math.random() * (500 - 200) + 200;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    this.x += this.speed * dt;
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
 };
 
 // Now write your own player class
