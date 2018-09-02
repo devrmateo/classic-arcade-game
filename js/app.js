60d885732283a83cab4c68fd3a8043251e68ca29
Matthew Smith <mateoelcule@gmail.com>
2018-09-02 15:24:37 -0700

    Wrote Enemy.prototype.update function.

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x += this.speed * dt;
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
